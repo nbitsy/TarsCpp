fe5e59e3ba2975a9781e46a61bd672814decc0fc
nbitsy <nbits.y@163.com>
2026-06-09 11:57:18 +0800

    生成的文件也提交

--- clear-install.cmake
+++ clear-install.cmake
@@ -1,1 +1,2 @@
-EXECUTE_PROCESS(COMMAND C:/Program Files/CMake/bin/cmake.exe -E remove_directory C:/tars/cpp/include)
+#EXECUTE_PROCESS(COMMAND C:/Program Files/CMake/bin/cmake.exe -E remove_directory C:/tars/cpp/include)
+EXECUTE_PROCESS(COMMAND /usr/bin/cmake -E remove_directory /data/app/include)
